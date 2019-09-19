412ca1048ebe69d889efee6bfd7c028b728fd609
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-09-19 00:34:29 -0400

    made a route

---
+++ Flask/static/flaskapp.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print(__name__)
+    print("HELOOOOOOOOOOOO")
+    return "No hablo queso!"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

4d7c0792957966062b662507f70faad0e8559093
Joseph Lee <jlee01@stuy.edu>
2019-09-19 01:07:35 -0400

    made 3 app routes

--- Flask/flaskapp.py
+++ Flask/flaskapp.py
@@ -7,6 +7,18 @@ def hello_world():
     print("HELOOOOOOOOOOOO")
     return "No hablo queso!"
 
+@app.route("/<name>")
+def hello_world_name(name):
+    print(__name__)
+    print("HELOOOOOOOOOOOO")
+    return "No hablo queso " + name + "!"
+
+@app.route("/rude")
+def hello_world_rude():
+    print(__name__)
+    print("HELOOOOOOOOOOOO")
+    return "fuck off"
+
 if __name__ == "__main__":
     app.debug = True
     app.run()

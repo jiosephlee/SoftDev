ebf9ff6c8e9c3345b06dd0589db5390157067bd9
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-09-23 08:48:03 -0400

    progress

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,3 +1,8 @@
+#<Frist> <Lsat>
+#SoftDev1 pd<p>
+#K<n> -- <Title/Topic/Summary>
+#<yyyy>-<mm>-<dd>
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -9,9 +14,30 @@ def hello_world():
     return "No hablo queso!"
 
 @app.route("/occupyflaskst")
-def test_templt():
+
+def occ_template():
     print(__name__)
-    return render_template('template.html',title = "Team Seven's Homepage")
+    #Turning CSV File into Dictionary
+    dict = {}
+    csv = open("jobs.csv","r")
+    data = open("jobs.csv").read().split("\n")
+    data = data[1:len(data)-2]
+    #print(data)
+    for item in data:
+        input = item.rsplit(",",1)
+        #print(input)
+        dict[input[0]] = [float(input[1])]
+    #print(dict)
+    csv.close()
+    threshold = float(random.randint(1,100))
+    random_occ = ""
+    for item in dict:
+        value = dict[item][0]
+        if (threshold - value <= 0):
+            random_occ = ""
+        else:
+            threshold = threshold - value
+    return render_template('template.html',title = "Team Seven's Homepage", occ = random_occ, dict_jobs = dict)
 
 if __name__ == "__main__":
     app.debug = True

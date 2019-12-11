ea163877e6baeb5bf71f13df44606d328cef35a7
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-12-10 21:34:14 -0500

    transfered school work to personal directroy

---
+++ 27_js/funky_town.js
@@ -0,0 +1,39 @@
+var food = function(n){
+	print("foo");
+};
+
+
+var fact = function(n){
+    if (n < 2){
+        return 1;}
+    else {
+        return (n * (fact(n - 1)));}
+
+}
+
+var fib = function(n){
+    if (n = 0){
+        return 0;
+    }
+    if (n = 1){
+        return 1;
+    }
+    else{
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+var GCD = function (a,b){
+    var big;
+    if (a < b){
+        big = b;
+    }
+    else{
+        big = a;
+    }
+    var ind;
+    for (ind = 0; ind < )
+}
+
+-- randStudent
+console.log(fib(3))
